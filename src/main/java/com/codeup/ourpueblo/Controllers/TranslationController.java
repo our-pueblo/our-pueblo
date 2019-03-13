79115880194622ad16d045d89ae74ff27c68dbdc
Ryan Glazer <rglazer003@gmail.com>
2019-03-13 16:58:06 -0500

    Filling out missing templates and getmappings

--- src/main/java/com/codeup/ourpueblo/Controllers/TranslationController.java
+++ src/main/java/com/codeup/ourpueblo/Controllers/TranslationController.java
@@ -1,9 +1,9 @@
 package com.codeup.ourpueblo.Controllers;
 
+import com.codeup.ourpueblo.Models.Translation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TranslationController {
@@ -12,9 +12,38 @@ public class TranslationController {
         return "request_translation";
     }
 
+    @GetMapping("/translate/viewall")
+    public String viewAllTranslations(){
+        return "translate_viewall";
+    }
+
+    @GetMapping("/translate/view")
+    public String viewTranslation(@RequestParam long id){
+        return "view_translation";
+    }
+
     @GetMapping("/translate/{id}")
     public String translate(@PathVariable long id, Model model){
         model.addAttribute("request_id", id);
         return "translate";
+    }
+
+    //TODO Write translation repository
+    @PostMapping("/translate")
+    public String submitTranslation (@ModelAttribute Translation translation){
+        //TODO Come back after repository
+        return "/";
+    }
+
+    @PostMapping("/translate/delete")
+    public String deleteTranslation(@RequestParam long deleteID){
+        //TODO Come back after repository
+        return "/";
+    }
+
+    @PostMapping ("/translate/edit")
+    public String editTranslation(@ModelAttribute Translation editedTranslation){
+        //TODO Come back after repository
+        return "/";
     }
 }
