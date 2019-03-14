51fb79d0b66269ec4859f5b9f986dd677dcdca22
Ryan Glazer <rglazer003@gmail.com>
2019-03-14 10:37:03 -0500

    Working on repostiories and doing test forms and postmappings as I go

--- src/main/java/com/codeup/ourpueblo/Controllers/RequestController.java
+++ src/main/java/com/codeup/ourpueblo/Controllers/RequestController.java
@@ -1,10 +1,18 @@
 package com.codeup.ourpueblo.Controllers;
 
+import com.codeup.ourpueblo.RequestRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RequestController {
+
+    private final RequestRepository requestDao;
+
+    public RequestController(RequestRepository requestDao){
+        this.requestDao = requestDao;
+    }
+
     @GetMapping("/request")
     public String makeRequest(){
         return "request";
---
+++ src/main/java/com/codeup/ourpueblo/RequestRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.ourpueblo;
+
+import com.codeup.ourpueblo.Models.Request;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RequestRepository extends CrudRepository<Request, Long> {
+}
