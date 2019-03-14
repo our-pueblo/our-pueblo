fc686a9e9908c8307df540d1e5f0a0fe8b2efb0c
Ryan Glazer <rglazer003@gmail.com>
2019-03-14 14:39:48 -0500

    Repositories are done, register and request test forms are done

---
+++ src/main/java/com/codeup/ourpueblo/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.ourpueblo;
+
+import com.codeup.ourpueblo.Models.Department;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DepartmentRepository extends CrudRepository<Department, Long> {
+}
---
+++ src/main/resources/seeding.sql
@@ -0,0 +1,5 @@
+use pueblo_db;
+insert into departments (name)
+values ('Test Department');
+
+insert into request_status (id, status) VALUES (101, 'Test Request Status')
