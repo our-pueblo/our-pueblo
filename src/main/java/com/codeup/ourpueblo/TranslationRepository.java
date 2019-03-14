72b121b3a5b254160b049c213874806ed12847d5
Ryan Glazer <rglazer003@gmail.com>
2019-03-14 16:56:20 -0500

    More reposittories and the translation form and post method

---
+++ src/main/java/com/codeup/ourpueblo/TranslationRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.ourpueblo;
+
+import com.codeup.ourpueblo.Models.Translation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TranslationRepository extends CrudRepository<Translation, Long> {
+}
